de14b6f8fd8fdfb5952b0e97d2a295c8444450d9
ranjithvunnam <reddy.rkranjith@gmail.com>
2017-10-10 20:46:27 +0530

    Gitignore added

--- source/WP-Web/src/main/java/org/product/warranty/pro/web/restservices/RestAPIController.java
+++ source/WP-Web/src/main/java/org/product/warranty/pro/web/restservices/RestAPIController.java
@@ -1,9 +1,17 @@
 package org.product.warranty.pro.web.restservices;
 
+import org.json.JSONObject;
+import org.product.warranty.pro.services.handlers.WPServiceException;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller("/warranty/api/")
+@Controller("/warranty/api/v0.1/")
 public class RestAPIController {
 	
-	
+	@RequestMapping(value = {"/getAdmins"}, method = RequestMethod.GET)
+	public @ResponseBody String getAdmins() throws WPServiceException {
+		return JSONObject.quote("Hello World");
+	}
 }
